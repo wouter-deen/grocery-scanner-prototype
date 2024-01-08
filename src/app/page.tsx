2c6c0a915841265ee46c4aadd65e5a6e570f9464
wouter-deen <wouter@wouterdeen.nl>
2024-01-08 12:03:25 +0100

    Image scanner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,59 @@ export default function Home() {
   const [currentTab, setCurrentTab] = useState(0);
   const [toggle, setToggle] = useState(0);
 
+  function runScan() {
+    const file = document.querySelector('input[type=file]')?.files[0];
+
+    const PAT = 'c1d7cdd443654f16bc7a64d3d77d3b88';
+// Specify the correct user_id/app_id pairings
+// Since you're making inferences outside your app's scope
+    const USER_ID = 'clarifai';
+    const APP_ID = 'main';
+// Change these to whatever model and image URL you want to use
+    const MODEL_ID = 'food-item-recognition';
+    const MODEL_VERSION_ID = '1d5fd481e0cf4826aa72ec3ff049e044';
+    const IMAGE_URL = window.URL.createObjectURL(file)
+    alert(IMAGE_URL)
+
+///////////////////////////////////////////////////////////////////////////////////
+// YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+///////////////////////////////////////////////////////////////////////////////////
+
+    const raw = JSON.stringify({
+      "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+      },
+      "inputs": [
+        {
+          "data": {
+            "image": {
+              "url": IMAGE_URL
+            }
+          }
+        }
+      ]
+    });
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+      },
+      body: raw
+    };
+
+// NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
+// https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
+// this will default to the latest version_id
+
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+      .then(response => response.text())
+      .then(result => alert(result))
+      .catch(error => alert('error: ' + error));
+  }
+
   return (
     <main className="px-4 py-4 sm:px-8 sm:py-8">
       <h3 className="text-5xl sm:text-6xl font-bold">Your Food</h3>
@@ -49,12 +102,18 @@ export default function Home() {
       <div className="hidden sm:flex justify-center absolute bottom-8 left-1/2">
         <div className="backdrop-blur-md bg-black/10 w-fit relative -left-1/2 px-4 py-3 rounded-lg">
           <div className="flex justify-center space-x-3 mb-2">
-            <IconButton icon={FaPlus} className={toggle === 0 ? "bg-lime-600" : "bg-neutral-400"} onClick={() => setToggle(0)}/>
-            <IconButton icon={FaMinus} className={toggle === 1 ? "bg-lime-600" : "bg-neutral-400"} onClick={() => setToggle(1)}/>
+            <IconButton icon={FaPlus} className={toggle === 0 ? "bg-lime-600" : "bg-neutral-400"}
+                        onClick={() => setToggle(0)}/>
+            <IconButton icon={FaMinus} className={toggle === 1 ? "bg-lime-600" : "bg-neutral-400"}
+                        onClick={() => setToggle(1)}/>
           </div>
-          <Button className="bg-lime-600">Open Scanner</Button>
+          <Button className="bg-lime-600" onClick={() => document.getElementById("camInput")?.click()}>Open Scanner</Button>
+          <input type="file" accept="image/*" capture="camera" className="hidden" id="camInput" onChangeCapture={() => runScan()}/>
         </div>
       </div>
+
+      <img/>
+
     </main>
   )
 }
